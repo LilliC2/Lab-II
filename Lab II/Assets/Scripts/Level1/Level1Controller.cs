c7d0508f13c38d1de5033b25aa953e634821243d
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-02-26 21:48:33 +1100

    Build error fix

--- Lab II/Assets/Scripts/Level1/Level1Controller.cs
+++ Lab II/Assets/Scripts/Level1/Level1Controller.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.Build;
 
 public class Level1Controller : Singleton<Level1Controller>
 {
--- Lab II/Assets/Scripts/Level1/PuzzlePieceLocations.cs
+++ Lab II/Assets/Scripts/Level1/PuzzlePieceLocations.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
