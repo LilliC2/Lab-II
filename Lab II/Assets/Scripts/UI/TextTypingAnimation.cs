84e7ef2db1ff365b35e0a9409af40369081f64d9
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-03-20 12:58:40 +1100

    BillyPush
    Before he ruins it

---
+++ Lab II/Assets/Scripts/UI/TextTypingAnimation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextTypingAnimation : GameBehaviour
+{
+    public TextMeshPro text_TMP;
+
+    //IEnumerator Start()
+    //{
+    //    text_TMP = gameObject.GetComponent<TextMeshPro>() ?? gameObject.AddComponent<TextMeshPro>();
+
+    //    int totalVisibleCharacters = text_TMP.textInfo.characterCount;
+    //    int counter = 0;
+    //}
+}
