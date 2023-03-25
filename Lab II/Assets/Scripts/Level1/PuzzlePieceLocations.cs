f3b3c2811f14934863393ff7bd388b5d2377e7bf
Lilli <lillichuawiwat@gmail.com>
2023-03-25 11:25:28 +1100

    Fixed Layer 3 Pick Up

--- Lab II/Assets/Scripts/Level1/PuzzlePieceLocations.cs
+++ Lab II/Assets/Scripts/Level1/PuzzlePieceLocations.cs
@@ -101,7 +101,6 @@ public class PuzzlePieceLocations : Singleton<PuzzlePieceLocations>
                     snapped = false;
                     index = j;
 
-                    print(_puzzlePieces[j].transform.eulerAngles.y);
 
 #region aniamtion
                     if (Vector3.Distance(_puzzlePieces[j].transform.position, _puzzlePiecesEndPos[j].transform.position) < baseRange)
@@ -293,7 +292,7 @@ public class PuzzlePieceLocations : Singleton<PuzzlePieceLocations>
 
                 //puzzlePiecesL3[i].gameObject.transform.localScale = new Vector3(2, 2, 2);
 
-                puzzlePiecesL3[i].gameObject.transform.position = (new Vector3(UnityEngine.Random.Range(69.9f, 52.9f), 5, UnityEngine.Random.Range(-5.5f, 7.9f)));
+                puzzlePiecesL3[i].gameObject.transform.position = (new Vector3(UnityEngine.Random.Range(69.9f, 52.9f), 8, UnityEngine.Random.Range(-5.5f, 7.9f)));
 
                 int num = rotations[UnityEngine.Random.Range(0, 3)];
                 puzzlePiecesL3[i].gameObject.transform.eulerAngles = new Vector3(0, Mathf.Round(num), 0);
